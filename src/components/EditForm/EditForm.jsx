8dd6ee1cd2be62b6d3ca13329632e77e7ceeb300
Siv Earley <searley96@gmail.com>
2023-04-25 15:10:09 -0500

    added pngs to images folder

--- src/components/EditForm/EditForm.jsx
+++ src/components/EditForm/EditForm.jsx
@@ -2,48 +2,58 @@ import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import React, { useState } from "react";
 import { useEffect } from "react";
+import "./EditForm.css";
 
 function EditForm() {
   const dispatch = useDispatch();
   const history = useHistory();
 
-  const [editURL, setEditURL] = useState('')
+  const [editURL, setEditURL] = useState("");
 
-//   const editImage = useSelector((store) => store.editReducer);
+  //   const editImage = useSelector((store) => store.editReducer);
 
-  const user = useSelector((store => store.user))
+  const user = useSelector((store) => store.user);
 
-   //gets image from user table
-   useEffect(() => {
-    dispatch({ type: 'FETCH_IMAGE' });
+  //gets image from user table
+  useEffect(() => {
+    dispatch({ type: "FETCH_IMAGE" });
   }, []);
 
-
-const editItem = (incomingURL) => {
+  const editItem = (incomingURL) => {
     // first check the URL input; if blank, change the URL to the existing one
-    if (editURL === '') {
-        setEditURL(incomingURL)
-      }
-      dispatch({
-        type: 'EDIT_IMAGE',
-        payload: {
-            userId: user.id,
-          image_url: editURL,
-        }
-      })
-}
+    if (editURL === "") {
+      setEditURL(incomingURL);
+    }
+    dispatch({
+      type: "EDIT_IMAGE",
+      payload: {
+        userId: user.id,
+        image_url: editURL,
+      },
+    });
+  };
 
   return (
-    <>
-      <h1>Edit Image</h1>
-      <h1 id='user-header'>Welcome, {user.username}!</h1>
-      <img src={user.image_url} />
-      
-      <form onSubmit={() => editItem(user.id, user.image_url)} id= {user.id}>
-      <input placeholder="new image url" value={editURL} onChange={(event) => setEditURL(event.target.value)} />
-      <button type="submit" >Submit Edit</button>
-      </form>
-    </>
+    <div id="edit-paper">
+      <div id="pattern">
+        <div id="content">
+          <h1>Edit Image</h1>
+          <h1 id="user-header">Welcome, {user.username}!</h1>
+          <img src={user.image_url} />
+
+          <form onSubmit={() => editItem(user.id, user.image_url)} id={user.id}>
+            <input
+              placeholder="New Image URL"
+              value={editURL}
+              onChange={(event) => setEditURL(event.target.value)}
+            />
+            <button id="submit-bttn" type="submit">
+              Save
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/SpiralPage/SpiralPage.jsx
@@ -0,0 +1,3 @@
+import { gsap } from "gsap";
+import { PixiPlugin } from "gsap/PixiPlugin.js";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin.js";
